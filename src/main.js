048f0e190c4874f95fbe97b687298caef3c10688
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2025-03-31 14:52:49 +0200

    Visual Demo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import './assets/main.sass'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,106 @@
 </script>
 
 <template>
-  <main>
-    Home
+  <main class="wrapper">
+    <p>
+      paragraph
+    </p>
+    <h1 class="border a">
+      Heading 1
+    </h1>
+    <h2 class="border l">
+      Heading 2
+    </h2>
+    <h3 class="border t">
+      Heading 3
+    </h3>
+    <h4 class="border r">
+      Heading 4
+    </h4>
+    <h5 class="border b">
+      Heading 5
+    </h5>
+    <h6>
+      Heading 6
+    </h6>
+
+    <p>
+      <a href="">
+        Link
+      </a>
+    </p>
+
+    <p>
+      <a href="" class="CTA">
+        Link
+      </a>
+    </p>
+
+    <div class="linkBox">
+      <a href="">
+        Link
+      </a>
+    </div>
+
+    <div class="linkBox CTA">
+      <a href="">
+        Link
+      </a>
+    </div>
+
+    <article class="border a">
+      <h1 class="border b">
+        Title!
+      </h1>
+      <p>
+        Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla 
+      </p>
+      <p class="dc">
+        Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla 
+      </p>
+      <h3>
+        Sub Title!
+      </h3>
+      <p>
+        Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla 
+      </p>
+    </article>
+
+    <section>
+      <article class="border a col-6">
+        <h1 class="border b">
+          Title!
+        </h1>
+        <p>
+          Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla 
+        </p>
+        <p class="dc">
+          Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla 
+        </p>
+        <h3>
+          Sub Title!
+        </h3>
+        <p>
+          Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla 
+        </p>
+      </article>
+      <article class="border a col-6">
+        <h1 class="border b">
+          Title!
+        </h1>
+        <p>
+          Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla 
+        </p>
+        <p class="dc">
+          Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla 
+        </p>
+        <h3>
+          Sub Title!
+        </h3>
+        <p>
+          Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla 
+        </p>
+      </article>
+    </section>
   </main>
 </template>
