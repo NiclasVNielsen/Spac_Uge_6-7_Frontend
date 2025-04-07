d4d93b7f542b9d47b99cb332ce92fef60a3ea538
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2025-04-07 09:45:01 +0200

    prettier cards

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -6,16 +6,20 @@ const props = defineProps(['product'])
 
 <template>
     <div class="border h card">
-        <img src="@/media/ph.jpg" alt="">
-        <h6>
-            {{ props.product.name }}
-        </h6>
-        <p>
-            {{ props.product.category }}
-        </p>
+        <div class="imgContainer">
+            <img src="@/media/ph.jpg" alt="">
+        </div>
+        <article>
+            <h6>
+                {{ props.product.name }}
+            </h6>
+            <p>
+                {{ props.product.category }}
+            </p>
+        </article>
         <form action="" class="flex">
-            <input type="text" value="1" class="amount">
-            <input type="submit" value="Add to Cart" class="submit">
+            <input type="text" @focus="function(e){e.srcElement.select()}" value="1" class="amount">
+            <input type="submit" value="Add to Cart" class="submit linkBox">
         </form>
     </div>
 </template>
@@ -25,21 +29,35 @@ const props = defineProps(['product'])
 .card
     display: inline-block
     width: var(--cardSize)
-    text-align: center
     cursor: pointer
     padding: var(--sameContextGap)
-    h6
-        margin-top: var(--sameContextGap)
+    text-align: center
+    .imgContainer
+        border: solid var(--borderSize) var(--prim)
+        border-radius: var(--borderRadius)
+        img
+            display: block
+    article
+        padding: var(--sameContextGap)
+        h6
+            margin-top: var(--sameContextGap)
     .flex
         margin-bottom: 0
-        input
-            width: 50%
-            border-color: var(--second)
         .amount
             text-align: right
             border-radius: var(--borderRadius) 0 0 var(--borderRadius)
+            width: 33.333%
+            border-right: calc(var(--borderSizeLight) / 2) solid var(--prim)
+            color: var(--second)
         .submit
             border-radius: 0 var(--borderRadius) var(--borderRadius) 0
+            width: 66.666%
+            border-left: calc(var(--borderSizeLight) / 2) solid var(--prim)
+            color: var(--neutral)
+            border: solid var(--borderSizeLight)  var(--prim) 
+            &:hover
+                color: var(--prim)
+                border: solid var(--borderSizeLight)  var(--prim)
 
 
 
