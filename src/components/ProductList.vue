27d47bab752586fcfc8ed48a80273051f12939fe
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2025-04-09 12:55:54 +0200

    arrow hover effect on productlist

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -167,14 +167,14 @@ const toggleShowAll = () => {
     </div>
     <div class="dotContainer" :id="'productsDots' + props.id" v-if="!showAll">
         <div @click="scrollToId(onLeft ?  currentLocation - 1 : currentLocation - amount, 'prod' + props.id, 'productsDots' + props.id, null)" class="chevron">
-            <svg fill="#01BAEF" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
         </div>
       <template v-for="(product, index) in productsShortList" :key="index">
         <div @click="event => {scrollToId(index, 'prod' + props.id, 'productsDots' + props.id, event)}" class="dot" :class="'dot' + props.id">
         </div>
       </template>
         <div @click="scrollToId(onLeft ? currentLocation + amount : currentLocation + 1, 'prod' + props.id, 'productsDots' + props.id, null)" class="chevron">
-            <svg fill="#01BAEF" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
         </div>
     </div>
     <div class="showAllList" v-if="showAll">
@@ -267,5 +267,13 @@ h4
         fill: purple
         color: orange
         user-select: none
+
+
+div:hover
+    >svg
+        fill: var(--prim)
+svg
+    transition: var(--quickTransition)
+    fill: var(--second)
 
 </style>
